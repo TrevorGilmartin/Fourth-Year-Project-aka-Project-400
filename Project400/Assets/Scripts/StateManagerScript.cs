a0641ab4e19586ab164d257e076cd89faad02d46
DESKTOP-BGPNJNR\trevo <trevorgilmartin1995@gmail.com>
2018-02-26 11:02:58 +0000

    After corrupted Repo re-upload of the Fourth Year Project making this Mk.3

---
+++ Project400/Assets/Scripts/StateManagerScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum TurnsState
+{
+    PlayerTurn_1,
+    PlayerTurn_2,
+    PlayerTurn_3,
+    EnemyTurn_1,
+    EnemyTurn_2,
+    EnemyTurn_3,
+}
+
+public enum States
+{
+
+}
+
+public class StateManagerScript : MonoBehaviour
+{
+    public static StateManagerScript instance;
+
+    public TurnsState currentTurnState = TurnsState.PlayerTurn_1;
+    // Use this for initialization
+    void Start()
+    {
+        currentTurnState = TurnsState.PlayerTurn_1;
+        instance = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+
+    public void ChangingTurnState()
+    {
+        currentTurnState = TurnsState.EnemyTurn_1;
+    }
+}
